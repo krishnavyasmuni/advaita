7111b435dec9b9591b12832486f0c43b71c79c7e
Vyas Muni <319807238+krishnavyasmuni@users.noreply.github.com>
2026-09-17 17:17:14 +0100

    Complete Bhagavatam Canto 11 Chapter 8 source data

--- assets/js/bhagavatam-sridhara-clarity.js
+++ assets/js/bhagavatam-sridhara-clarity.js
@@ -30,6 +30,7 @@
       ,'/vivekadrishti/assets/data/bhagavatam-sridhara-wfw-canto11-ch05-verses01-52.json?v=20260917-1'
       ,'/vivekadrishti/assets/data/bhagavatam-sridhara-wfw-canto11-ch06-verses01-50.json?v=20260917-1'
       ,'/vivekadrishti/assets/data/bhagavatam-sridhara-wfw-canto11-ch07-verses01-74.json?v=20260917-1'
+      ,'/vivekadrishti/assets/data/bhagavatam-sridhara-wfw-canto11-ch08-verses01-44.json?v=20260917-1'
     );
   }
 
